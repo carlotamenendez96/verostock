6ccc13719da0f5566ba0c1ed84c518eb42ee4f6e
carlota-home <carlotamenendezalvarez96@gmail.com>
2026-02-06 20:59:44 +0100

    Implement scroll-to-top functionality in BlogLayout on route change using useEffect and useLocation hooks.

--- src/features/blog/layouts/BlogLayout.tsx
+++ src/features/blog/layouts/BlogLayout.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export function BlogLayout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="flex flex-col min-h-screen bg-white text-deep-black">
       <Navbar />
